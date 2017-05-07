9ba2bd1b4dcd5f84f123ea7e5e52ccc5c470549c
kth <wwwholakr@gmail.com>
2017-05-07 14:28:51 +0900

    bx slider lazy 완료 js 기능별 분기 준비

---
+++ src/common/common.js
@@ -0,0 +1,91 @@
+(function(a,b,c,d){var e=a(b);a.fn.lazyload=function(c){function i(){var b=0;f.each(function(){var c=a(this);if(h.skip_invisible&&!c.is(":visible"))return;if(!a.abovethetop(this,h)&&!a.leftofbegin(this,h))if(!a.belowthefold(this,h)&&!a.rightoffold(this,h))c.trigger("appear"),b=0;else if(++b>h.failure_limit)return!1})}var f=this,g,h={threshold:0,failure_limit:0,event:"scroll",effect:"show",container:b,data_attribute:"original",skip_invisible:!0,appear:null,load:null};return c&&(d!==c.failurelimit&&(c.failure_limit=c.failurelimit,delete c.failurelimit),d!==c.effectspeed&&(c.effect_speed=c.effectspeed,delete c.effectspeed),a.extend(h,c)),g=h.container===d||h.container===b?e:a(h.container),0===h.event.indexOf("scroll")&&g.bind(h.event,function(a){return i()}),this.each(function(){var b=this,c=a(b);b.loaded=!1,c.one("appear",function(){if(!this.loaded){if(h.appear){var d=f.length;h.appear.call(b,d,h)}a("<img />").bind("load",function(){c.hide().attr("src",c.data(h.data_attribute))[h.effect](h.effect_speed),b.loaded=!0;var d=a.grep(f,function(a){return!a.loaded});f=a(d);if(h.load){var e=f.length;h.load.call(b,e,h)}}).attr("src",c.data(h.data_attribute))}}),0!==h.event.indexOf("scroll")&&c.bind(h.event,function(a){b.loaded||c.trigger("appear")})}),e.bind("resize",function(a){i()}),/iphone|ipod|ipad.*os 5/gi.test(navigator.appVersion)&&e.bind("pageshow",function(b){b.originalEvent.persisted&&f.each(function(){a(this).trigger("appear")})}),a(b).load(function(){i()}),this},a.belowthefold=function(c,f){var g;return f.container===d||f.container===b?g=e.height()+e.scrollTop():g=a(f.container).offset().top+a(f.container).height(),g<=a(c).offset().top-f.threshold},a.rightoffold=function(c,f){var g;return f.container===d||f.container===b?g=e.width()+e.scrollLeft():g=a(f.container).offset().left+a(f.container).width(),g<=a(c).offset().left-f.threshold},a.abovethetop=function(c,f){var g;return f.container===d||f.container===b?g=e.scrollTop():g=a(f.container).offset().top,g>=a(c).offset().top+f.threshold+a(c).height()},a.leftofbegin=function(c,f){var g;return f.container===d||f.container===b?g=e.scrollLeft():g=a(f.container).offset().left,g>=a(c).offset().left+f.threshold+a(c).width()},a.inviewport=function(b,c){return!a.rightoffold(b,c)&&!a.leftofbegin(b,c)&&!a.belowthefold(b,c)&&!a.abovethetop(b,c)},a.extend(a.expr[":"],{"below-the-fold":function(b){return a.belowthefold(b,{threshold:0})},"above-the-top":function(b){return!a.belowthefold(b,{threshold:0})},"right-of-screen":function(b){return a.rightoffold(b,{threshold:0})},"left-of-screen":function(b){return!a.rightoffold(b,{threshold:0})},"in-viewport":function(b){return a.inviewport(b,{threshold:0})},"above-the-fold":function(b){return!a.belowthefold(b,{threshold:0})},"right-of-fold":function(b){return a.rightoffold(b,{threshold:0})},"left-of-fold":function(b){return!a.rightoffold(b,{threshold:0})}})})(jQuery,window,document)
+/**
+ * Created by Publish Team on 2017-04-26.
+ */
+;(function ($, win) {
+	'use strict';
+	var $win = $(win);
+
+	/**
+	 * [uuid - unique id 명 생성]
+	 * @param null
+	 * @return {String} 고유한 id 값을 반환한다.
+	 */
+	$.uuid = function () {
+		var lut = []; for (var i=0; i<256; ++i) { lut[i] = (i<16?'0':'')+(i).toString(16); }
+
+		var d0 = Math.random()*0xffffffff|0,
+			d1 = Math.random()*0xffffffff|0,
+			d2 = Math.random()*0xffffffff|0,
+			d3 = Math.random()*0xffffffff|0
+		;
+		return lut[d0&0xff]+lut[d0>>8&0xff]+lut[d0>>16&0xff]+lut[d0>>24&0xff]+'-'+
+			lut[d1&0xff]+lut[d1>>8&0xff]+'-'+lut[d1>>16&0x0f|0x40]+lut[d1>>24&0xff]+'-'+
+			lut[d2&0x3f|0x80]+lut[d2>>8&0xff]+'-'+lut[d2>>16&0xff]+lut[d2>>24&0xff]+
+			lut[d3&0xff]+lut[d3>>8&0xff]+lut[d3>>16&0xff]+lut[d3>>24&0xff];
+	};
+
+	/**
+	 * [ inScreen 뷰포트 영역캐치]
+	 * @param  {jQuery}  $el           [위치 확인 대상 - 뷰포트 영역에 들어갔는지 여부를 확인하고 싶은 대상 객체.]
+	 * @return {Boolean} true/false    [ 대상 객체가 스크린에 보이는지 아닌지에 대한 여부 ]
+	 */
+	$.inScreen = function ($el) {
+		if(!$el.length) return;
+
+		var scrollLeft  = $win.scrollLeft(),
+			scrollTop   = $win.scrollTop(),
+			offset      = $el.offset(),
+			offsetY     = 10,
+			rect1 = { x1: scrollLeft, y1: scrollTop + offsetY, x2: scrollLeft + $win.width(), y2: scrollTop + $win.height() - offsetY },
+			rect2 = { x1: offset.left, y1: offset.top, x2: offset.left + $el.width(), y2: offset.top + $el.height() }
+		;
+		var stance = {
+			left: offset.left - scrollLeft,
+			top: offset.top - scrollTop,
+			insideViewport: rect1.x1 < rect2.x2 && rect1.x2 > rect2.x1 && rect1.y1 < rect2.y1 && rect1.y2 > rect2.y1,
+			crossAtTop: rect2.y1 < rect1.y1 && rect2.y1 < rect1.y2 && rect2.y2 > rect1.y1 && rect2.y1 < rect1.y2,
+			crossAtBottom: rect2.y1 > rect1.y1 && rect2.y1 < rect1.y2 && rect2.y2 > rect1.y1 && rect2.y2 > rect1.y2
+		};
+		return stance.insideViewport || stance.crossAtTop || stance.crossAtBottom;
+	};
+
+	/**
+	 * [ loadLazyed bx 슬라이더용 lazyload 설정 ]
+	 * @param {Object} settings
+	 * 					{String} originAttr       : 원래 이미지의 path 를 가지고 있는 속성명
+	 * 					{String} lazySelector     : lazy 가 적용 될 dom(img) 노드의 jQuery 선택자 문자열
+	 * 					{String} shouldRemoveAttr : lazy 적용후 삭제 해야할 dom 속성명
+	 * 					{Function} onComplete     : lazy 적용후 실행 callback
+	 * @returns {Object} jQuery self
+	 */
+	$.fn.loadLazyed = function (settings) {
+		var options = $.extend(true, {
+			exposeAll: false,
+			dataAttrName: 'data-original',
+			lazySelector: '.bx-lazy',
+			shouldRemoveAttr: 'width height style',
+			onComplete: null
+		}, settings || {});
+
+		return this.each(function () {
+			var $this = $(this),
+				$lazyTarget, path
+			;
+			if(!$this.is('img')) {
+				$lazyTarget = $this.find(options.lazySelector);
+			}
+			else { $lazyTarget = $this; }
+
+			path = $lazyTarget.attr(options.dataAttrName);
+			if(path) {
+				$lazyTarget.attr('src', path).removeAttr(options.dataAttrName+' '+options.shouldRemoveAttr);
+			}
+			if($.type(options.onComplete) === 'function') {
+				$lazyTarget.load(function() {
+					options.onComplete.call($this);
+				})
+			}
+		});
+	};
+})(jQuery, window);
---
+++ src/lazyload3/lazyload3.js
@@ -0,0 +1,67 @@
+/**
+ * Created by Hackers Publish Team on 2017-04-26.
+ */
+;(function ($, win) {
+    'use strict';
+    var $win = $(win);
+    /**
+     * 순차적 이미지 로딩을 위한 lazy 전역함수
+     * scrollTop 0 으부터 스크롤시 정상동작, 하단으로부터 스크롤 혹은 페이지 로드시 앵커태그로 임의 이동의 경우 정상적으로 동작하지 않음.
+     * usage - $.lazy(selectors);
+     * @param $elements
+     */
+    $.lazy = function ($elements) {
+        var $els = $($elements);
+
+        var c = {
+            $cur: '',
+            inProgress: false,
+            check: function () {
+                if(c.inProgress) return false;
+
+                $els.each(function () {
+                    var $this = $(this);
+                    if($.inScreen($this)) {
+                        c.inProgress = true;
+                        $this.loadLazyed({onComplete: function () {
+                            var $el = $($els.splice(0, 1));
+                            $el.removeClass('lazy').removeAttr('data-original');
+                            c.inProgress = false;
+                            c.check();
+                        }});
+                        return false;
+                    }
+                })
+            }
+        };
+        $win.scroll(function () {
+            c.check();
+        });
+    };
+    /**
+     * [ lazyload3 ]
+     * @desc	lazyload(http://appelsiini.net/projects/lazyload/) 가 기본적으로 대상 이미지에 height 를 선언해주어야 하는 부분에 대한 보완
+     * 			min-height 를 임의로 선언함으로 대상 이미지가 스크린에 노출/로드 되는지에 대한 여부를 결정할수 있도록 설정.
+     * 			해당 이미지가 로드된 이후에는 min-height 속성을 삭제함으로써 설정된 min-height보다 작은 이미지에 대한 대응.
+     *
+     * @param {Object} settings {
+         * 						{int}       minHeight : 스크롤 포지션확정을 위한 이미지의 최소 가정 높이( 해당 페이지의 이미지 평균높이에 따라 임의로 설정가능)
+         * 						{Boolean}	forceSize : min-height 를 무시하고 width, height 를 직접 대상에 입력할시 true 로 설정하면 min-height 값을 무시한다.
+     * @returns {jQuery} self;
+     */
+    $.fn.lazyload3 = function (settings) {
+        var options = $.extend(true, {
+            minHeight: $win.height(),
+            forceSize: false
+        }, settings || {});
+
+        return this.each(function () {
+            var $this = $(this);
+            if(!options.forceSize) {$this.height(options.minHeight).css('display', 'block')}
+
+            $this.lazyload().load(function () {
+                $this.removeAttr('style data-original');
+            });
+        })
+    }
+})(jQuery, window);
